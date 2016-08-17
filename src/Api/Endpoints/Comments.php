4f46e1e5f68ef60f279498e54f9a4a499481ebd7
Zhuk Sergey <seregazhuk88@gmail.com>
2016-08-17 20:57:13 +0300

    add: Comments endpoint

---
+++ src/Api/Endpoints/Comments.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace seregazhuk\Favro\Api\Endpoints;
+
+use seregazhuk\Favro\Api\Endpoints\Traits\CrudEndpoint;
+use seregazhuk\Favro\Api\Endpoints\Traits\BelongsToCard;
+use seregazhuk\Favro\Api\Endpoints\Traits\BelongsToOrganization;
+
+class Comments extends Endpoint {
+
+	use CrudEndpoint, BelongsToOrganization, BelongsToCard;
+
+	/**
+	 * @var string
+	 */
+	protected $endpoint = 'comments';
+}
