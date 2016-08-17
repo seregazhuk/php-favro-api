2c89d5300d59a79c9bbedf638356c5d2c4734b99
Zhuk Sergey <seregazhuk88@gmail.com>
2016-08-17 20:56:05 +0300

    add: TasksLists endpoint

--- src/Api/Endpoints/Tags.php
+++ src/Api/Endpoints/Tags.php
@@ -6,7 +6,11 @@ use seregazhuk\Favro\Api\Endpoints\Traits\BelongsToOrganization;
 use seregazhuk\Favro\Api\Endpoints\Traits\CrudEndpoint;
 
 class Tags extends Endpoint {
+
 	use CrudEndpoint, BelongsToOrganization;
 
+	/**
+	 * @var string
+	 */
 	protected $endpoint = 'tags';
 }
---
+++ src/Api/Endpoints/TaskLists.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace seregazhuk\Favro\Api\Endpoints;
+
+use seregazhuk\Favro\Api\Endpoints\Traits\CrudEndpoint;
+use seregazhuk\Favro\Api\Endpoints\Traits\BelongsToCard;
+use seregazhuk\Favro\Api\Endpoints\Traits\BelongsToOrganization;
+
+class TaskLists extends Endpoint {
+
+	use CrudEndpoint, BelongsToOrganization, BelongsToCard;
+
+	/**
+	 * @var string
+	 */
+	protected $endpoint = 'tasklists';
+}
