fce46796ec3127ba1a9b8ac94d7d491d57ad2e0b
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-08-19 18:46:18 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Api/Endpoints/Endpoint.php
+++ src/Api/Endpoints/Endpoint.php
@@ -6,93 +6,93 @@ use seregazhuk\Favro\Contracts\HttpInterface;
 
 class Endpoint
 {
-    /**
-     * @var array
-     */
-    protected $allowedMethods = [
-        'getById',
-        'getAll',
-        'create',
-        'update',
-        'delete',
-    ];
+	/**
+	 * @var array
+	 */
+	protected $allowedMethods = [
+		'getById',
+		'getAll',
+		'create',
+		'update',
+		'delete',
+	];
 
-    /**
-     * @var string
-     */
-    protected $endpoint;
+	/**
+	 * @var string
+	 */
+	protected $endpoint;
 
-    /**
-     * @var array
-     */
-    protected $headers = [];
+	/**
+	 * @var array
+	 */
+	protected $headers = [];
 
-    /**
-     * @var HttpInterface
-     */
-    protected $http;
+	/**
+	 * @var HttpInterface
+	 */
+	protected $http;
 
-    /**
-     * @param HttpInterface $http
-     */
-    public function __construct(HttpInterface $http)
-    {
-        $this->http = $http;
-    }
+	/**
+	 * @param HttpInterface $http
+	 */
+	public function __construct(HttpInterface $http)
+	{
+		$this->http = $http;
+	}
 
-    /**
-     * @param string $verb
-     * @return string
-     */
-    public function makeRequestUrl($verb = '')
-    {
-        return "https://favro.com/api/v1/{$this->endpoint}/$verb";
-    }
+	/**
+	 * @param string $verb
+	 * @return string
+	 */
+	public function makeRequestUrl($verb = '')
+	{
+		return "https://favro.com/api/v1/{$this->endpoint}/$verb";
+	}
 
-    /**
-     * @param string $method
-     * @return bool
-     */
-    public function isMethodAllowed($method)
-    {
-        return in_array($method, $this->allowedMethods);
-    }
+	/**
+	 * @param string $method
+	 * @return bool
+	 */
+	public function isMethodAllowed($method)
+	{
+		return in_array($method, $this->allowedMethods);
+	}
 
-    /**
-     * @return HttpInterface
-     */
-    public function getHttp()
-    {
-        return $this->http;
-    }
+	/**
+	 * @return HttpInterface
+	 */
+	public function getHttp()
+	{
+		return $this->http;
+	}
 
-    /**
-     * @param array $params
-     * @return array
-     */
-    public function getAll(array $params = [])
-    {
-        return $this->getHttp()->get(
-            $this->makeRequestUrl(), $params, $this->getHeaders()
-        );
-    }
+	/**
+	 * @param array $params
+	 * @return array
+	 */
+	public function getAll(array $params = [])
+	{
+		return $this->getHttp()->get(
+			$this->makeRequestUrl(), $params, $this->getHeaders()
+		);
+	}
 
-    /**
-     * @param string $id
-     * @return array
-     */
-    public function getById($id)
-    {
-        return $this->getHttp()->get(
-            $this->makeRequestUrl($id), [], $this->getHeaders()
-        );
-    }
+	/**
+	 * @param string $id
+	 * @return array
+	 */
+	public function getById($id)
+	{
+		return $this->getHttp()->get(
+			$this->makeRequestUrl($id), [], $this->getHeaders()
+		);
+	}
 
-    /**
-     * @return array
-     */
-    protected function getHeaders()
-    {
-        return $this->headers;
-    }
+	/**
+	 * @return array
+	 */
+	protected function getHeaders()
+	{
+		return $this->headers;
+	}
 }
