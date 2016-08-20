f40236fe08b5e3e986eeb58f765bd17b285bba99
Zhuk Sergey <seregazhuk88@gmail.com>
2016-08-20 10:57:02 +0300

    upd: codeStyle

--- src/Api/Endpoints/Traits/CrudEndpoint.php
+++ src/Api/Endpoints/Traits/CrudEndpoint.php
@@ -1,52 +1,52 @@
 <?php
 
-
 namespace seregazhuk\Favro\Api\Endpoints\Traits;
 
-trait CrudEndpoint {
+trait CrudEndpoint
+{
 
-	/**
-	 * @param array $attributes
-	 * @return array
-	 */
-	public function create(array $attributes)
-	{
-		return $this
-			->getHttp()
-			->post(
-				$this->makeRequestUrl(),
-				$attributes,
-				['organizationId' => $this->organizationId]
-			);
-	}
+    /**
+     * @param array $attributes
+     * @return array
+     */
+    public function create(array $attributes)
+    {
+        return $this
+            ->getHttp()
+            ->post(
+                $this->makeRequestUrl(),
+                $attributes,
+                ['organizationId' => $this->organizationId]
+            );
+    }
 
-	/**
-	 * @param string $id
-	 * @param array $attributes
-	 * @return mixed
-	 */
-	public function update($id, array $attributes)
-	{
-		return $this
-			->getHttp()
-			->put(
-				$this->makeRequestUrl($id),
-				$attributes,
-				['organizationId' => $this->organizationId]
-			);
-	}
+    /**
+     * @param string $itemId
+     * @param array $attributes
+     * @return mixed
+     */
+    public function update($itemId, array $attributes)
+    {
+        return $this
+            ->getHttp()
+            ->put(
+                $this->makeRequestUrl($itemId),
+                $attributes,
+                ['organizationId' => $this->organizationId]
+            );
+    }
 
-	/**
-	 * @param string $id
-	 * @return mixed
-	 */
-	public function delete($id)
-	{
-		return $this
-			->getHttp()
-			->delete(
-				$this->makeRequestUrl($id),
-				['organizationId' => $this->organizationId]
-			);
-	}
+    /**
+     * @param string $itemId
+     * @return mixed
+     */
+    public function delete($itemId)
+    {
+        return $this
+            ->getHttp()
+            ->delete(
+                $this->makeRequestUrl($itemId),
+                ['organizationId' => $this->organizationId]
+            );
+    }
 }
