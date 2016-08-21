509b28aedd88cc6a182d11788cae4b66ac259edc
Zhuk Sergey <seregazhuk88@gmail.com>
2016-08-21 22:36:12 +0300

    add: EndpointsContainer tests

---
+++ tests/EndpointsContainerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace seregazhuk\tests;
+
+use GuzzleHttp\Client;
+use seregazhuk\Favro\Api\Endpoints\Users;
+use seregazhuk\Favro\Adapters\GuzzleHttpAdapter;
+use seregazhuk\Favro\Api\Endpoints\EndpointsContainer;
+
+class EndpointsContainerTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_resolves_an_instance_of_endpoint()
+    {
+        $container = $this->createContainer();
+        $this->assertInstanceOf(Users::class, $container->resolveEndpoint('users'));
+    }
+
+    /**
+     * @test
+     * @expectedException \seregazhuk\Favro\Exceptions\BadEndpointException
+     */
+    public function it_throws_exception_when_resolving_non_existing_endpoint()
+    {
+        $this->createContainer()
+            ->resolveEndpoint('unknown');
+    }
+
+    /**
+     * @return EndpointsContainer
+     */
+    protected function createContainer()
+    {
+        return new EndpointsContainer(new GuzzleHttpAdapter(new Client()));
+    }
+}
