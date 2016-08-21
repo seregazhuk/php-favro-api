6726161f4b5b647d18ad6e40e352dcfe85d11959
Zhuk Sergey <seregazhuk88@gmail.com>
2016-08-21 22:12:32 +0300

    add: FavroTest

--- tests/FavroTest.php
+++ tests/FavroTest.php
@@ -12,4 +12,16 @@ class FavroTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf(Api::class, Favro::create('login', 'password'));
     }
+
+    public function it_has_private_constructor()
+    {
+        $reflection = new \ReflectionClass(Favro::class);
+        $this->assertFalse($reflection->getConstructor()->isPublic());
+    }
+
+    public function it_has_private_clone_method()
+    {
+        $reflection = new \ReflectionClass(Favro::class);
+        $this->assertFalse($reflection->getMethod('__clone')->isPublic());
+    }
 }
